3d4e398d459c888fade0518dc91d5d32d74c5865
[jwhong70] <[jwhong70@gmail.com]>
2022-11-29 16:31:49 +0900

    #5 atom

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,5 +1,17 @@
+import { atom } from "recoil";
+
 export enum Categories {
   "TO_DO" = "TO_DO",
   "DOING" = "DOING",
   "DONE" = "DONE",
 }
+export interface IToDo {
+  text: string;
+  id: number;
+  category: Categories;
+}
+export const toDoState = atom<IToDo[]>({ key: "toDo", default: [] });
+export const categoryState = atom<Categories>({
+  key: "category",
+  default: Categories.TO_DO,
+});
