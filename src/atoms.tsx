19833f5de51c178db92fa9d3054498a158f7469b
[jwhong70] <[jwhong70@gmail.com]>
2022-11-29 20:37:09 +0900

    #9 Category Select

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export enum Categories {
   "TO_DO" = "TO_DO",
@@ -14,4 +14,12 @@ export const toDoState = atom<IToDo[]>({ key: "toDo", default: [] });
 export const categoryState = atom<Categories>({
   key: "category",
   default: Categories.TO_DO,
+});
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
+  },
 });
