62f3ba1f178da31c517102763b4c2ea377c675e5
[jwhong70] <[jwhong70@gmail.com]>
2022-11-29 17:35:11 +0900

    #7 ToDoList Layout

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,4 +1,19 @@
-function ToDo() {
-  return null;
+import { Categories, IToDo } from "../atoms";
+
+function ToDo({ text, category, id }: IToDo) {
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== Categories.DOING && (
+        <button name={Categories.DOING}>Doing</button>
+      )}
+      {category !== Categories.TO_DO && (
+        <button name={Categories.TO_DO}>To Do</button>
+      )}
+      {category !== Categories.DONE && (
+        <button name={Categories.DONE}>Done</button>
+      )}
+    </li>
+  );
 }
 export default ToDo;
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,4 +1,19 @@
+import { useRecoilValue } from "recoil";
+import { toDoState } from "./../atoms";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
+
 function ToDoList() {
-  return null;
+  const toDos = useRecoilValue(toDoState);
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <CreateToDo />
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
+    </div>
+  );
 }
 export default ToDoList;
